f7515acaf3b6220c50a2d3f5fe496c11076b96a4
touhidul7 <touhidul2@hotmail.com>
2025-08-23 02:01:05 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
+
 import MainLayout from "./Layouts/MainLayout";
 import Home from "./Pages/Home";
 import ForumPage from "./Pages/ForumPage";
